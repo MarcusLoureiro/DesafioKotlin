3359b8ed2526a531b045d29e22cbd55b784de3c2
MarcusLoureiro <marcushuriel08@hotmail.com>
2020-09-25 16:15:09 -0300

    Adicionando Classe Aluno

---
+++ src/com/digitalhouse/aula13/desafio/Aluno.kt
@@ -0,0 +1,8 @@
+package com.digitalhouse.aula13.desafio
+
+class Aluno (val nome: String, val sobreneome: String, val codigo: Int){
+
+    override fun equals(other: Any?): Boolean {
+        return super.equals(other)
+    }
+}
