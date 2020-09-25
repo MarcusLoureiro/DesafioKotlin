d6e7bfc4a58ca386b40e4cbb2a1266147d57ef1e
MarcusLoureiro <marcushuriel08@hotmail.com>
2020-09-25 17:02:41 -0300

    Implementando classe digitalHouseManager (Incompleta)

--- src/com/digitalhouse/aula13/desafio/DigitalHouseManager.kt
+++ src/com/digitalhouse/aula13/desafio/DigitalHouseManager.kt
@@ -1,4 +1,61 @@
 package com.digitalhouse.aula13.desafio
 
-class DigitalHouseManager {
+import java.util.*
+
+class DigitalHouseManager() {
+    val listaAlunos = mutableListOf<Aluno>()
+    val listaProfessores = mutableListOf<Professor>()
+    val listaCursos = mutableListOf<Curso>()
+    val listaMatriculas = mutableListOf<Matricula>()
+
+    fun registrarCurso(nome: String, codigoCurso: Int, qtdMaxAlunos: Int, professorAdjunto: ProfessorAdjunto, professorTitular: ProfessorTitular) {
+        val cursoNovo = Curso(nome, codigoCurso, professorTitular, professorAdjunto, qtdMaxAlunos)
+        listaCursos.add(cursoNovo)
+    }
+
+    fun excluirCurso(codigoCurso: Int) {
+        for (curso in listaCursos) {
+            if (curso.codigo == codigoCurso) {
+                listaCursos.remove(curso)
+            }
+        }
+    }
+
+    fun registrarProfessorAdjunto(nome: String, sobrenome: String, codigoProfessor: Int, qtdHoras: Int) {
+        val novoProfessor = ProfessorAdjunto(nome, sobrenome, 0, codigoProfessor, qtdHoras)
+        listaProfessores.add(novoProfessor)
+    }
+
+    fun registrarProfessorTitular(nome: String, sobrenome: String, codigoProfessor: Int, especialidade: String) {
+        val novoProfessor = ProfessorTitular(nome, sobrenome, 0, codigoProfessor, especialidade)
+        listaProfessores.add(novoProfessor)
+    }
+
+    fun excluirProfessor(codigoProfessor: Int) {
+        for (professor in listaCursos) {
+            if (professor.codigo == codigoProfessor) {
+                listaProfessores.remove(professor)
+            }
+        }
+    }
+
+    fun registrarAluno(nome: String, sobrenome: String, codigoAluno: Int) {
+        val novoAluno = Aluno(nome, sobrenome, codigoAluno)
+        listaAlunos.add(novoAluno)
+    }
+
+    fun matricularAluno(codigoAluno: Int, codigoCurso: Int){
+        var alunoNovo: Aluno
+        var cursoNovo: Curso
+        var data = Date()
+        for(aluno in listaAlunos){
+            if(aluno.codigo == codigoAluno){
+                 alunoNovo = aluno
+            }
+        }
+        for(curso in listaCursos){
+            if(curso.codigo == codigoCurso)
+                 cursoNovo = curso
+        }
+    }
 }
--- src/com/digitalhouse/aula13/desafio/ProfessorAdjunto.kt
+++ src/com/digitalhouse/aula13/desafio/ProfessorAdjunto.kt
@@ -1,6 +1,6 @@
 package com.digitalhouse.aula13.desafio
 
-class ProfessorAdjunto(nome: String, sobrenome: String, tempoCasa: Int, codigo: Int, qtdMonitoria: String): Professor(nome, sobrenome, tempoCasa, codigo) {
+class ProfessorAdjunto(nome: String, sobrenome: String, tempoCasa: Int, codigo: Int, qtdMonitoria: Int): Professor(nome, sobrenome, tempoCasa, codigo) {
 
     override fun equals(other: Any?): Boolean {
         return super.equals(other)
